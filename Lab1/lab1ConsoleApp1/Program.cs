d032c0cd4f90c975817f49f9010bcb82eb09ab41
tsekottt <tsekot.mariana@lll.kpi.ua>
2025-05-07 17:05:23 +0300

    Added comments for XML documentation

---
+++ Lab1/lab1ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// Основний клас програми.
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// Головний метод, з якого починається виконання програми.
+    /// </summary>
+    /// <param name="args">Аргументи командного рядка.</param>
+    static void Main(string[] args)
+    {
+        // Виводить повідомлення "Hello, World!" у консоль.
+        Console.WriteLine("Hello, World!");
+    }
+}
